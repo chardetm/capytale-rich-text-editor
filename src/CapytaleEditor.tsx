fa8548d1b88cc44914938e81d92fea820e4211af
Maverick Chardet <chardetm@gmail.com>
2023-10-09 18:29:04 +0200

    Handling json size limit

--- src/CapytaleEditor.tsx
+++ src/CapytaleEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 
 import EditorComposer from "./EditorComposer";
 import Editor from "./Editor";
@@ -10,19 +10,35 @@ import { $insertNodes, $getRoot, EditorState } from "lexical";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 
 import "./CapytaleEditor.css";
+import { useEditorContext } from "./contexts/EditorContext";
+
+const byteSize = (str) => new Blob([str]).size;
 
 export interface ICapytaleEditorContentProps {
   isEditable?: boolean;
   useLexicalContextMenu?: boolean;
   onChange?: () => void;
   onJsonChange?: (json: string) => void;
+  onJsonSizeLimitExceeded?: () => void;
+  onJsonSizeLimitMet?: () => void;
+  jsonSizeLimit?: number;
+  sizeCheckInterval?: number;
   htmlInitialContent?: string;
 }
+
 const CapytaleEditorContent: React.FC<ICapytaleEditorContentProps> = ({
   isEditable,
   useLexicalContextMenu,
   onChange,
   onJsonChange,
+  onJsonSizeLimitExceeded = () => {
+    alert(
+      `Le contenu de l'éditeur est trop volumineux. Veuillez le réduire avant de l'enregistrer.\nPeut-être avez-vous inséré une image trop volumineuse ?`
+    );
+  },
+  onJsonSizeLimitMet,
+  jsonSizeLimit,
+  sizeCheckInterval,
   htmlInitialContent,
 }) => {
   const [initialized, setInitialized] = React.useState(false);
@@ -40,8 +56,51 @@ const CapytaleEditorContent: React.FC<ICapytaleEditorContentProps> = ({
     }
   }, [htmlInitialContent, initialized]);
 
+  const capytaleRichTextEditorContext = useEditorContext();
+
+  const [jsonTooBig, setJsonTooBig] = React.useState(false);
+
+  const checkIfTooBig = useCallback(async () => {
+    const state = await capytaleRichTextEditorContext.getState(true, false);
+    if (!state) {
+      return;
+    }
+
+    if (byteSize(state.json) > jsonSizeLimit) {
+      if (!jsonTooBig) {
+        setJsonTooBig(true);
+        capytaleRichTextEditorContext.setCanSave &&
+          capytaleRichTextEditorContext.setCanSave(false);
+        setTimeout(() => {
+          onJsonSizeLimitExceeded && onJsonSizeLimitExceeded();
+        }, 1);
+      }
+    } else {
+      if (jsonTooBig) {
+        setTimeout(() => {
+          onJsonSizeLimitMet && onJsonSizeLimitMet();
+        }, 1);
+        setJsonTooBig(false);
+        capytaleRichTextEditorContext.setCanSave &&
+          capytaleRichTextEditorContext.setCanSave(true);
+      }
+    }
+  }, [jsonTooBig, capytaleRichTextEditorContext, jsonSizeLimit]);
+
+  useEffect(() => {
+    if (
+      !sizeCheckInterval ||
+      !jsonSizeLimit ||
+      !capytaleRichTextEditorContext
+    ) {
+      return;
+    }
+    const interval = setInterval(checkIfTooBig, sizeCheckInterval);
+    return () => clearInterval(interval);
+  }, [jsonTooBig, capytaleRichTextEditorContext, jsonSizeLimit]);
+
   return (
-    <div className="editor-inner">
+    <div className={"editor-inner" + (jsonTooBig ? " too-big" : "")}>
       <Editor
         draggableBlocksEnabled={true}
         hashtagsEnabled={true}
@@ -73,6 +132,10 @@ export interface ICapytaleEditorProps {
   useLexicalContextMenu?: boolean;
   onChange?: () => void;
   onJsonChange?: (json: string) => void;
+  onJsonSizeLimitExceeded?: () => void;
+  onJsonSizeLimitMet?: () => void;
+  jsonSizeLimit?: number;
+  sizeCheckInterval?: number;
   initialEditorState?: InitialEditorStateType;
   htmlInitialContent?: string;
 }
@@ -82,6 +145,10 @@ const CapytaleEditor: React.FC<ICapytaleEditorProps> = ({
   useLexicalContextMenu = false,
   onChange,
   onJsonChange,
+  onJsonSizeLimitExceeded,
+  onJsonSizeLimitMet,
+  jsonSizeLimit,
+  sizeCheckInterval = 2000,
   initialEditorState,
   htmlInitialContent,
 }): React.ReactElement => {
@@ -93,6 +160,10 @@ const CapytaleEditor: React.FC<ICapytaleEditorProps> = ({
       <CapytaleEditorContent
         onChange={onChange}
         onJsonChange={onJsonChange}
+        onJsonSizeLimitExceeded={onJsonSizeLimitExceeded}
+        onJsonSizeLimitMet={onJsonSizeLimitMet}
+        jsonSizeLimit={jsonSizeLimit}
+        sizeCheckInterval={sizeCheckInterval}
         isEditable={isEditable}
         useLexicalContextMenu={useLexicalContextMenu}
         htmlInitialContent={htmlInitialContent}
--- src/contexts/EditorContext.tsx
+++ src/contexts/EditorContext.tsx
@@ -4,16 +4,20 @@ import { createContext, ReactNode, useContext, useState } from "react";
 type StateGetter = (
   needJson?: boolean,
   needHtml?: boolean
-) => Promise<{ html: null|string; json: null|string }>;
+) => Promise<{ html: null | string; json: null | string }>;
 
 type ContextShape = {
   getState?: StateGetter;
   setGetState?: (f: () => StateGetter) => void;
+  canSave?: boolean;
+  setCanSave?: (b: boolean) => void;
 };
 
 const Context: React.Context<ContextShape> = createContext({
   getState: null,
   setGetState: null,
+  canSave: true,
+  setCanSave: null,
 });
 
 export const EditorContext = ({
@@ -22,11 +26,14 @@ export const EditorContext = ({
   children: ReactNode;
 }): JSX.Element => {
   const [getState, setGetState] = useState<StateGetter>(null);
+  const [canSave, setCanSave] = useState<boolean>(true);
   return (
     <Context.Provider
       value={{
         getState: getState,
         setGetState: setGetState,
+        canSave: canSave,
+        setCanSave: setCanSave,
       }}
     >
       {children}
